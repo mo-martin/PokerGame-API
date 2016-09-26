60259237955e56b5a3ad225fcbba3bc967668456
nathan oconnor <nathan.oconnor@skybettingandgaming.com>
2016-09-26 15:59:31 +0100

    expanded game schema

---
+++ gameEngine.js
@@ -0,0 +1,27 @@
+var Game = require('../models/game');
+
+function takeBet(BetInfo)
+{
+    //bet info JSON with gameID / player id / bet amount
+    Game.findById(BetInfo.id,function(err,result){
+        if(err) console.log(err);
+        //result == game object
+        //assuming front end validation
+        //maybe add checks with flash module
+        result.player[BetInfo.pid].bet = BetInfo.bet;
+        result.player[BetInfo.pid].cash -= BetInfo.bet;
+        result.pot += BetInfo.bet;
+        
+    });
+}
+function payOut()
+{
+    //get an integer with which player won
+    //special payout conditions
+
+    //need to check for an all in bet
+    
+    //if all in bet is not as high as the others , then cap their payout
+    var cappedWinners = []
+
+}
--- models/game.js
+++ models/game.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 
 var gameSchema = new mongoose.Schema(
 {
-    deck : {type : Array , required : true}
+    deck : {type : Array , required : true},
+    gameState : {type : Number, required : true},
+    players : {type : Array , required : true},
+    dealHand : {type : Array, required : true},
+    pot : {type : Number, required : true},
+    curBet : {type : Number, required : true}
 });
 
 module.exports = mongoose.model("Game",gameSchema);

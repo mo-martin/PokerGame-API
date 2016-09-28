136bb69a4f7d16c45012c383c07309ebfa1b749d
Alan Bonham <alan.bonham@skybettingandgaming.com>
2016-09-28 16:12:42 +0100

    cors security layer update

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var router = require('./config/routes');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var path = require('path');
+var cors = require('cors');
 
 var port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
